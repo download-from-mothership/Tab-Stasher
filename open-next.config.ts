ad4ccfb59b112604cc6c07af542f6a6e95108cac
download-from-mothership <alicia@kensicomgmt.com>
2025-07-18 23:35:17 -0400

    fix: update tailwind config and simplify opennext config for deployment

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,10 +1,5 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
-import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
-import doQueue from "@opennextjs/cloudflare/overrides/queue/do-queue";
-import doTagCache from "@opennextjs/cloudflare/overrides/tag-cache/do-sharded-tag-cache";
 
 export default defineCloudflareConfig({
-  incrementalCache: r2IncrementalCache,
-  queue: doQueue,
-  tagCache: doTagCache,
+  // Simplified config for deployment
 });
